9412699b4535bce3113c60ba62a37479d11ee904
quanhaom <quan67100@gmail.com>
2026-09-18 00:31:52 +0700

    biutiphun

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,27 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
+
 import "maplibre-gl/dist/maplibre-gl.css";
 import "./globals.css";
 
 const montserrat = Montserrat({
   subsets: ["latin", "vietnamese"],
   display: "swap",
+
   variable: "--font-montserrat",
+
+  weight: [
+    "400",
+    "500",
+    "600",
+    "700",
+    "800",
+  ],
 });
 
 export const metadata: Metadata = {
   title: "Bản đồ tìm kiếm hài cốt liệt sĩ",
+
   description:
     "Bản đồ tương tác về các địa điểm tìm kiếm và phát hiện hài cốt liệt sĩ tại Việt Nam.",
 };
